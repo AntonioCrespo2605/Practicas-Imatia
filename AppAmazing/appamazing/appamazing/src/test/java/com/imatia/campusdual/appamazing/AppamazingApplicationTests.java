c2db24d991b27316478dbecfd60ec71fade61ad7
Antonio Crespo <antoniocrespgome@gmail.com>
2023-05-02 10:55:38 +0200

    adding appamazing

---
+++ AppAmazing/appamazing/appamazing/src/test/java/com/imatia/campusdual/appamazing/AppamazingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imatia.campusdual.appamazing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppamazingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
